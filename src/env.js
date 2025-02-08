9e90b3f5101bac4f947baf02de1d27a6d0caef72
OviedoF <oviedofederico039@gmail.com>
2025-02-07 22:39:52 -0300

    Tamaños en moderador

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
 export default {
-    //API_URL: "https://my-print.app/api",
-    API_URL: "http://localhost:4000/api",
+    API_URL: "https://my-print.app/api",
+    //API_URL: "http://localhost:4000/api",
 } 
